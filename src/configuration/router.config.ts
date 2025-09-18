cb25080dbf043075e6963d056328445348def922
Muntazir Manan <muntazircodes@gmail.com>
2025-09-18 21:12:33 +0530

    health route added

--- src/configuration/router.config.ts
+++ src/configuration/router.config.ts
@@ -1,6 +1,7 @@
 import { Express } from 'express';
 import authRoutes from '../routes/auth.routes';
-
+import healthRoutes from '../routes/health.routes';
 export const initRouter = (app: Express) => {
     app.use('/api/auth', authRoutes);
+    app.use('/api/v1/health', healthRoutes);
 };
---
+++ src/routes/health.routes.ts
@@ -0,0 +1,14 @@
+import { Request, Response, Router } from 'express';
+
+const router = Router();
+
+/**
+ *  @method             GET
+ *  @description        the health endpoint check
+ *  @access             public
+ */
+router.get('/', (req: Request, res: Response) => {
+    res.sendStatus(200);
+});
+
+export default router;
